c82e00df97b64b85afb0536f36c9650c864387d2
holberton <cavidansgrv06@gmail.com>
2025-11-22 11:45:05 -0500

    task

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -7,7 +7,6 @@ This module contains a class Square that defines a square by its size.
 class Square:
     """Represents a square with a private size attribute."""
 
-
     def __init__(self, size=0):
         """
         Initialize the square with a given size.
@@ -22,6 +21,7 @@ class Square:
 
     @size.setter
     def size(self, value):
+        """ Set the size of square"""
         if not isinstance(value,  int):
             raise TypeError("size must be an integer")
         if value < 0:
