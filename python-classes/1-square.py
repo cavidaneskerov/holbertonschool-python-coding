14967b4a17216b0c32f41118102847e70404dca5
holberton <cavidansgrv06@gmail.com>
2025-11-22 11:17:18 -0500

    task

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -8,7 +8,7 @@ class Square:
 
     """ Returns size """
     def __init__(self, size=0):
-        if not isinstance(size, (int,float)):
+        if not isinstance(size, (int)):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
