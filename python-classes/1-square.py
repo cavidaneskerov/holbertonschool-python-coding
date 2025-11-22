76699aae1e60a5d46a055b122d5f2b1880ba3ab0
holberton <cavidansgrv06@gmail.com>
2025-11-22 11:16:05 -0500

    task

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contain square size
+"""
+
+
+class Square:
+
+    """ Returns size """
+    def __init__(self, size=0):
+        if not isinstance(size, (int,float)):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
